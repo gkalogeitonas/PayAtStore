731a1101b484d8ff5dceb1c029c49a192352ef74
Giorgos Kalogeitonas <gkalog2@gmail.com>
2023-08-09 18:46:21 +0300

    change gitignore and remove some comment

--- catalog/model/extension/payment/payatstore.php
+++ catalog/model/extension/payment/payatstore.php
@@ -3,7 +3,7 @@ class ModelExtensionPaymentPayAtStore extends Model {
 	public function getMethod($address, $total) {
 		$this->load->language('extension/payment/payatstore');
 
-		//echo "<pre>";print_r($this->session->data['shipping_method']);echo "</pre>";
+
 		if ((isset($this->session->data['shipping_method']) && $this->session->data['shipping_method']['code']=='pickup.pickup') OR (isset($this->session->data['api_id']) && $this->session->data['api_id']==1)){
 		  $status  = true;
 	  }else{
